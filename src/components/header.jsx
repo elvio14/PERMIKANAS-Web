6a41b346e8537536101754734a50fdac0967b041
Tjioe Andrew Elvio Febrian <andrew.elvio14@gmail.com>
2025-03-13 17:04:35 -0400

    ✨ Contact buttons fix
    Buttons Copies email, Resources routes fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,15 @@
 "use client"
 import {MainButton, SecondaryButton} from '@/components/button.jsx'
-import Dropdown from './dropdown'
 import { useEffect, useState } from 'react'
 import { useRouter } from "next/navigation"
 
 export default function Header({active = "home"}){
 
     const [activePage, setActivePage] = useState("")
-
+    const [copyAlert, showCopyAlert] = useState(false)
     const router = useRouter()
 
-    const [scrollY, setScrollY] = useState(window.scrollY)
+    //const [scrollY, setScrollY] = useState(window.scrollY)  DEBUG SCROLL
     const [dropdown, setDropdown] = useState(false)
     const [floater, setFloater] = useState(false)
     const [isFadingOut, setFadeOut] = useState(false)
@@ -20,7 +19,7 @@ export default function Header({active = "home"}){
 
     useEffect(() => {
         const handleScroll = () => {
-            setScrollY(window.scrollY)
+            //setScrollY(window.scrollY)  DEBUG SCROLL
             if(window.scrollY >= 99){
                 setFloater(true)
                 setFadeOut(false)
@@ -45,6 +44,22 @@ export default function Header({active = "home"}){
         router.push(path)
     }
 
+    const clickContact = async ()=> {
+        // COPY EMAIL
+        try{
+          const text = "contact.permikanasional@gmail.com"
+          await navigator.clipboard.writeText(text)
+          showCopyAlert(true)
+        }catch(err){
+          console.error(err)
+        }
+        
+        // POPUP ALERT for 5s
+        setTimeout(()=>{
+          showCopyAlert(false)
+        }, 5000)
+    }
+
     useEffect(()=> {
         setActivePage(active)
     }, [])
@@ -54,13 +69,13 @@ export default function Header({active = "home"}){
         <>
         <div className="w-full h-[4.2rem] bg-[var(--main-3)] grid grid-cols-[1fr_2fr_1fr] items-center shadow-md z-40">
             <div className='flex justify-center'>   
-                <img style={{ borderRadius: '5rem'}} src="/permikanas_logo_white.svg" alt="Permkanas Logo" width={80}></img>
+                <img src="/permikanas_logo_white.svg" alt="Permkanas Logo" width={80}></img>
             </div>
             <div style={{paddingRight: '3rem'}}>
                 <nav className="flex gap-4 justify-center">
                     <SecondaryButton text='Home' color='white' isActive={activePage == "home"} onClick={()=>goToPage("/")}></SecondaryButton>
                     <SecondaryButton text='About Us' color='white' isActive={activePage == "about"} onClick={()=>goToPage("/about")}></SecondaryButton>
-                    <div onMouseEnter={resourceMouseEnter} onMouseLeave={resourceMouseLeave} className='relative'>
+                    <div onMouseEnter={resourceMouseEnter} onMouseLeave={resourceMouseLeave} onClick={()=>goToPage("/resources")} className='relative'>
                         
                         {dropdown && <div className='absolute top-[2.5rem] pl-8 py-4 rounded-b-xl bg-[var(--main-3)] w-[14rem] text-white shadow-lg' onMouseEnter={resourceMouseEnter} onMouseLeave={resourceMouseLeave}>
                             <h3>Before & Upon Arrival</h3>
@@ -76,14 +91,21 @@ export default function Header({active = "home"}){
 
                 </nav>
             </div>
-            <div className="flex justify-center">
-            <MainButton text='Contact Us'></MainButton>
+            <div className="flex justify-center relative">
+                <MainButton text='Contact Us' handleClick={clickContact}></MainButton>
+                {copyAlert && 
+                    <div className='absolute w-[12rem] top-[3rem] left-1/2 -translate-x-1/2 bg-[var(--main-3)] text-[var(--main-1)] p-2 shadow-md rounded'>Email copied!<br></br> Send us an email!</div>
+                }
             </div>
-            <div className='fixed'>{scrollY} - {floater ?  "TRUE" : "FALSE"}</div>
         </div>
+        {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */}
         {floater && 
-        <div className={`fixed top-[1rem] left-1/2 -translate-x-1/2 bg-white py-[0.5rem] w-[50vw] rounded-full shadow-lg z-50
-        ${isFadingOut ? "animate-fade-out opacity-0" : "animate-fade-in opacity-100"}`}>
+        <div className={`fixed top-[1rem] left-1/2 -translate-x-1/2 bg-white py-[0.5rem] w-[58vw] rounded-full shadow-lg z-50
+                        grid grid-cols-[1fr_4fr_1fr]
+                        ${isFadingOut ? "animate-fade-out opacity-0" : "animate-fade-in opacity-100"}`}>
+            <div className='flex justify-center'>   
+                <img src="/permikanas_logo_black.svg" alt="Permkanas Logo" className='w-[4rem]'></img>
+            </div>
             <nav className="flex gap-4 justify-center">
               <SecondaryButton text='Home' color='var(--main-7)' bgActive='#CD5A48' isActive={activePage == "home"} onClick={()=>goToPage("/")}></SecondaryButton>
               <SecondaryButton text='About Us' color='var(--main-7)' bgActive='#CD5A48' isActive={activePage == "about"} onClick={()=>goToPage("/about")}></SecondaryButton>
@@ -101,6 +123,12 @@ export default function Header({active = "home"}){
               <SecondaryButton text='Events' color='var(--main-7)' bgActive='#CD5A48' isActive={activePage == "events"} onClick={()=>goToPage("/events")}></SecondaryButton>
               <SecondaryButton text='Communities' color='var(--main-7)' bgActive='#CD5A48' isActive={activePage == "communities"} onClick={()=>goToPage("/communities ")}></SecondaryButton>
             </nav>
+            <div className="flex justify-center relative">
+                <MainButton text='Contact Us' borderColor="var(--main-6)" handleClick={clickContact}></MainButton>
+                {copyAlert && 
+                    <div className='absolute w-[8rem] top-full left-1/2 -translate-x-1/2 bg-white p-2 shadow-md rounded'>Email copied! Send us an email!</div>
+                }
+            </div>
         </div>
         }
         </>
