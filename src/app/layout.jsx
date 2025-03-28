4a2ad6f6baa2a2779f6406ce330884a88e78c4d0
Kamila0110 <145477218+kamilaaisya@users.noreply.github.com>
2025-03-27 22:45:56 -0400

    article fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,9 +29,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* {children}
-        <Footer></Footer> */}
-        <NotFound/>
+        {children}
+        <Footer></Footer> 
+        {/* <NotFound/> */}
       </body>
     </html>
   )
