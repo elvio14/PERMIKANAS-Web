9b99d7bb59c4eeac43841d5d1c711a9b8fb7d73e
VerrillAng <vaa13@sfu.ca>
2025-03-30 02:43:33 -0700

    add map to home

--- src/components/homeMap.jsx
+++ src/components/homeMap.jsx
@@ -1,13 +1,20 @@
 import CommHomeComponent from "./commHomeComponent"
+import Spline from '@splinetool/react-spline';
 
 export default function HomeMap() {
 
     return (
-        <div className="grid bg-[var(--main-1)]">
-            <div className="row-start-1 col-start-1 absolute right-0 h-full w-[60vw] z-10">
-                MAP 3D
+        <div className="relative bg-[var(--main-1)]">
+            
+            <div className="absolute right-0 h-full w-[60vw] right-[-7rem] z-10">
+                {/* MAP 3D */}
+                <Spline 
+                    scene="https://prod.spline.design/Xao3S9XZmSLC0tqy/scene.splinecode" 
+                />
             </div>
-            <CommHomeComponent/>
+            <div className="relative z-20 pointer-events-none">
+                <CommHomeComponent/>
+            </div>
         </div>
     )
 }
