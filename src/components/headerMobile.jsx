be38ea5888167764f16c94161cce43c03d1478a7
Tjioe Andrew Elvio Febrian <andrew.elvio14@gmail.com>
2025-03-30 10:46:17 -0400

    💄 Header mobile
    -

--- src/components/headerMobile.jsx
+++ src/components/headerMobile.jsx
@@ -1,8 +1,11 @@
+"use client"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
+import { MainButton } from "./button"
 
 export default function HeaderMobile({active = ""}) {
     const [showMenu, setShowMenu] = useState(false)
+    const [copyAlert, showCopyAlert] = useState(false)
     const router = useRouter()
     const goToPage = (path) => {
         console.log("Running goToPage " + path)
@@ -13,6 +16,23 @@ export default function HeaderMobile({active = ""}) {
     const toggleRes = () => {
         setOpenRes(!openRes)
     }
+
+    const clickContact = async ()=> {
+        // COPY EMAIL
+        try{
+          const text = "contact.permikanasional@gmail.com"
+          await navigator.clipboard.writeText(text)
+          showCopyAlert(true)
+        }catch(err){
+          console.error(err)
+        }
+        
+        // POPUP ALERT for 5s
+        setTimeout(()=>{
+          showCopyAlert(false)
+        }, 5000)
+    }
+
     return (
         <>
             <div className="fixed top-0 left-0 w-full bg-[var(--main-3)] z-40 grid grid-cols-[1fr_6fr_1fr] items-center">
@@ -22,9 +42,9 @@ export default function HeaderMobile({active = ""}) {
             </div>
             {showMenu &&
             <div className="w-full">
-                <div className="shadow-[10px_0_15px_rgba(0,0,0,0.25)] fixed h-full ml-[4rem] w-full bg-[var(--main-3)] z-50 py-8 flex flex-col gap-1 overflow-visible">
+                <div className="shadow-[10px_0_15px_rgba(0,0,0,0.25)] fixed h-full ml-[15vw] w-[85vw] bg-[var(--main-3)] z-50 py-8 flex flex-col gap-1 overflow-visible">
                     <div className="p-4">
-                        <h1 className="text-4xl text-[var(--main-1)] text-right mr-16" onClick={() => setShowMenu(false)}>{'>'}</h1>
+                        <h1 className="text-4xl text-[var(--main-1)] text-right pr-2" onClick={() => setShowMenu(false)}>{'>'}</h1>
                     </div>
                     <div className={`p-4 ${active == 'home' ? "bg-[var(--main-3-b)]" : "bg-transparent"}`}>
                         <h1 className="text-3xl text-[var(--main-1)]" onClick={() => goToPage("/")}>Home</h1>
@@ -47,6 +67,10 @@ export default function HeaderMobile({active = ""}) {
                     </div>
                     <div className={`p-4 ${active == 'communities' ? "bg-[var(--main-3-b)]" : "bg-transparent"}`}>
                         <h1 className="text-3xl text-[var(--main-1)]" onClick={() => goToPage("/communities")}>Communities</h1>
+                    </div>
+                    <div className="mt-auto flex flex-col items-center">
+                        {copyAlert && <p className="text-[var(--main-1)] p-2">Email address copied!</p>}
+                        <button onClick={() => clickContact()} className="bg-[var(--main-1)] rounded-full py-2 px-16 font-bold text-[var(--main-5)] text-xl">Contact Us</button>
                     </div>
                 </div>
             </div>}
