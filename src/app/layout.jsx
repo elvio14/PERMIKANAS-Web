0fd9520edcec829a9eaedbf6ae393ce30dc282ce
Andrew <andrew.elvio14@gmail.com>
2025-05-28 21:41:12 -0400

    📈 Added Vercel Analytics
    -

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 //import NotFound from "./not-found";
+import { Analytics } from "@vercel/analytics"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer></Footer>
         {/* <NotFound/>  */}
+        <Analytics/>
       </body>
     </html>
   )
