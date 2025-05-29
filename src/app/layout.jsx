a44ffb0b8e8037d9180de13c8424e030ec86b8c5
Andrew <andrew.elvio14@gmail.com>
2025-05-28 21:46:42 -0400

    📈 Fix import
    /next

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 //import NotFound from "./not-found";
-import { Analytics } from "@vercel/analytics"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
