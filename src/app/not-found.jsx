99f6113bea95894059ef1f5ced1facd2695b680b
Andrew <andrew.elvio14@gmail.com>
2025-06-03 15:21:22 -0400

    📱 Not Found Mobile Ver
    Image under 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,22 @@
+import useIsMobile from "@/components/isMobile"
+import Loading from "@/components/loading"
 
 export default function NotFound() {
+    let mobile = useIsMobile()
+    if (mobile === undefined){
+      return (<div className="pb-[12rem]"><Loading/></div>)
+    }
+
+    if(mobile){
+      return (
+        <div className="flex flex-col items-center justify-center text-center gap-4 px-[2rem] text-[var(--main-5)] p-8 py-32 animate-scaleUp">
+          <h1 className="manrope-h2 text-9xl">404</h1>
+          <img src="mobile-not-found.png" alt="temples" />
+          <h2 className="manrope-h2 text-4xl">You're Here Early!</h2>
+          <h4 className="manrope-thin text-xl">This page is still under construction. <br></br> Please come back shortly!</h4>
+        </div>
+      )
+    }
     return (
         <>
             <div className="relative z-0 md:h-[90vh] flex flex-col gap-4 p-8 pt-16  md:grid md:grid-cols-[1fr_1fr] md:px-[12rem] md:mb-[12rem] animate-scaleUp">
