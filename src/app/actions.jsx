1b9e9d81626c282398a803236e4a2c50d4334340
Tjioe Andrew Elvio Febrian <118779793+elvio14@users.noreply.github.com>
2025-06-30 14:22:15 -0400

    actions, gameStatuses

--- src/app/actions.jsx
+++ src/app/actions.jsx
@@ -5,22 +5,36 @@ import { db } from "./firebase"
 import { setDoc, doc } from "firebase/firestore"
 
 async function createMember(member){
-    try{
-      await setDoc(doc(db, "members", member.email), {
-        name: member.name,
-        email: member.email,
-        occupation: member.occupation,
-        institution: member.institution,
-        institutionOther: member.institutionOther,
-        city: member.city,
-        province: member.province,
-        timeAdded: Date.now()
-      })
-      console.log("User doc written with docref: ", member)
-    }catch(e){
-      console.error("Error adding document: ", e)
-    }
+  try{
+    await setDoc(doc(db, "members", member.email), {
+      name: member.name,
+      email: member.email,
+      occupation: member.occupation,
+      institution: member.institution,
+      institutionOther: member.institutionOther,
+      city: member.city,
+      province: member.province,
+      timeAdded: Date.now()
+    })
+    console.log("User doc written with docref: ", member)
+  }catch(e){
+    console.error("Error adding document: ", e)
   }
+}
 
+async function submitWordle(data){
+  try{
+    await setDoc(doc(db, "wordle", data.username), {
+      name: data.name,
+      wordleNumber: data.wordleNumber,
+      wordleStats: data.wordleStats,
+      chapter: data.chapter,
+      city: data.city,
+      timeAdded: Date.now(),
+    })
+  }catch(e){
+    console.error("Error submitting wordle: ",e)
+  }
+}
 
-export {createMember} 
+export {createMember, submitWordle} 
--- src/app/play/page.jsx
+++ src/app/play/page.jsx
@@ -1,17 +1,40 @@
 "use client"
 import Wordle from "@/components/wordle/wordle";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Play(){
-    const [gameStatus, setGameStatus] = useState("");
+    const [gameStatus, setGameStatus] = useState("play");
 
     const handleSignalFromWordle = (status) => {
         console.log("Signal received from Wordle:", status);
         setGameStatus(status); // Update parent state based on the signal
     };
+
+    const handleSolved = () => {
+
+    }
+
+    useEffect(()=> {
+        if(gameStatus === "solved"){
+            handleSolved()
+        }
+
+    }, [gameStatus])
     return (
         <div className="h-[100vh] pb-[12rem] h-fit">
-            <Wordle word="bakmi" onSignal={handleSignalFromWordle}/>
+            {gameStatus === "play" && <Wordle word="bakmi" onSignal={handleSignalFromWordle}/>}
+            
+            {gameStatus === "solved" && 
+                <div className="bg-['papayawhip'] w-[50vw] absolute">
+                    <h2>Cakep!</h2>
+                </div>
+            }
+
+            {gameStatus === "failed" &&
+                <div className="bg-['papayawhip'] w-[50vw] absolute">
+                    <h2>Coba lagi besok!</h2>
+                </div>
+            }
         </div>
     )
 }
